a9fe612274bb642b8d50904b9dcb318b070aa860
Barrolskii <ajbtdc@gmail.com>
2019-04-26 21:56:06 +0100

    Added a simple way to launch executables from a button press

---
+++ AshLauncher/AshLauncher/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace AshLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string result = ofd.FileName;
+
+                Process.Start(result);
+            }
+        }
+    }
+}
