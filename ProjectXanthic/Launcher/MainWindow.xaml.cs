3539533b448a78f956e3f9085ad0ea3ac4f99ea3
barrolskii <ajbtdc@gmail.com>
2019-07-27 22:55:24 +0100

    Added reading and writing functionality for program data and added a closing event handler

--- ProjectXanthic/Launcher/MainWindow.xaml.cs
+++ ProjectXanthic/Launcher/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.ComponentModel;
 
 namespace Launcher
 {
@@ -21,18 +23,16 @@ namespace Launcher
     public partial class MainWindow : Window
     {
 		List<ProgramData> programDataList;
+		string ProgramDataFilePath;
 
         public MainWindow()
         {
             InitializeComponent();
 
 			programDataList = new List<ProgramData>();
+			ProgramDataFilePath = "ProgramData.txt";
 
-			for (int i = 0; i < 20; i++)
-			{
-				ProgramData programData = new ProgramData("path " + i, "name " + i, "Img/DOTD.jpg");
-				programDataList.Add(programData);
-			}
+			ReadInData();
         }
 
 		private void Button_Click(object sender, RoutedEventArgs e)
@@ -70,6 +70,57 @@ namespace Launcher
 			debugTextBox.AppendText("\nindex: " + programDataList[index].ProgramName);
 
 			Background.DisplayImage.Source = new BitmapImage(new Uri(programDataList[index].BackgroundImagePath, UriKind.Relative));
+		}
+
+		private void ReadInData()
+		{
+			if (!File.Exists(ProgramDataFilePath))
+			{
+				FileStream fileStream = new FileStream(ProgramDataFilePath, FileMode.Create, FileAccess.ReadWrite);
+
+				debugTextBox.AppendText("Creating program data file\n");
+				fileStream.Close();
+
+				return;
+			}
+
+			string[] fileData = File.ReadAllLines(ProgramDataFilePath);
+
+			int i = 0;
+			foreach(string line in fileData)
+			{
+				string[] split = line.Split(',');
+
+				ProgramData data = new ProgramData(split[0], split[1], split[2]);
+				programDataList.Add(data);
+
+				Button button = new Button();
+				button.Name = "Button_" + i;
+				button.Content = "Button " + i;
+
+				button.Width = 300;
+				button.Height = 89.88;
+				button.HorizontalAlignment = HorizontalAlignment.Left;
+
+				button.Click += new RoutedEventHandler(ChangeDisplay);
+
+				StackPanelProgramList.Children.Add(button);
+
+				i++;
+			}
+		}
+
+		private void Topaz_Closing(object sender, CancelEventArgs e)
+		{
+			StreamWriter sw = new StreamWriter(ProgramDataFilePath);
+
+			foreach(ProgramData pd in programDataList)
+			{
+				string dataToWrite = pd.ProgramPath + ',' + pd.ProgramName + ',' + pd.BackgroundImagePath;
+				sw.WriteLine(dataToWrite);
+			}
+
+			sw.Close();
 		}
 	}
 }
